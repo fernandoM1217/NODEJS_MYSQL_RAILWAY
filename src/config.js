2b6c185efe0c6194f35ce02c60113000e278ad60
fer <fernan@andel.es>
2023-01-20 19:52:26 +0100

    primer commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 3000
+export const DB_HOST = process.env.DB_HOST || 'dpg-cf56i0la4992g5gcfm1g-a'
+export const DB_NAME = process.env.DB_NAME || 'userdb_ugoq'
+export const DB_USER = process.env.DB_USER || 'userdb_ugoq_user'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'CZyvKuB5aTIcquDfjQGgHXHzWryIGz5I' 
+export const DB_PORT = process.env.DB_PORT || 5432
